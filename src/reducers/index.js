8cb24ab9a9b0431fce372ff100eca8542fc1dcfe
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-10 18:49:35 +1100

    added reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'Numb', duration: '4:30' },
@@ -13,3 +15,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
