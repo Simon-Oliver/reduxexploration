88335a6aba3f5fbc340112ede583104f57537849
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-17 14:45:36 +1100

    minimal styling with Semantic UI

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,16 +8,23 @@ class PostList extends React.Component {
     fetchPosts();
   }
 
-  render() {
+  renderList() {
     const { posts } = this.props;
-    const renderList = posts.map(post => (
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.body}</p>
-        <br />
+    return posts.map(post => (
+      <div className="item" key={post.id}>
+        <i className="large middle aligned icon user" />
+        <div className="content">
+          <div className="description">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        </div>
       </div>
     ));
-    return <div>{renderList}</div>;
+  }
+
+  render() {
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
