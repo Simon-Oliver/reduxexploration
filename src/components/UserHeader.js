36fa1065b642604ae6607f5191d548e9f3443efd
Simon-Oliver <stauffer.simon@outlook.com>
2019-03-17 22:53:44 +1100

    added user reducer

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
+    const user = this.props.users.find(user => user.id === this.props.userId);
+
     return (
       <div>
-        <p>UserHeader</p>
+        <p>{user ? user.name : null}</p>
       </div>
     );
   }
 }
 
-export default UserHeader;
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
