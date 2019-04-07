2f188b9b73fa26165beac99560e9621a54070d86
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-07 13:13:18 +1000

    .env file with auth id

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        scope: 'email'
+      });
+    });
+  }
+
   render() {
     return <div>GoogleAuth</div>;
   }
