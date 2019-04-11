bd6931ac560fd15d3ce655b2b9e26c27bc0036c4
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-11 21:24:31 +1000

    wired up redux-form and basic inputs

--- client/src/components/video/VideoCreate.js
+++ client/src/components/video/VideoCreate.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class VideoCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return <input type="text" value={formProps.value} onChange={formProps.input.onChange} />;
+  }
+
   render() {
-    return <div>VideoCreate</div>;
+    return (
+      <div>
+        <form>
+          <Field name="title" component={this.renderInput} />
+          <Field name="description" component={this.renderInput} />
+        </form>
+      </div>
+    );
   }
 }
 
-export default VideoCreate;
+export default reduxForm({ form: 'streamCreate' })(VideoCreate);
