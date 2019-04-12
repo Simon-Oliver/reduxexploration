f995963b253b3d50b375ca44c81d1fc133f2bafb
Simon-Oliver <stauffer.simon@outlook.com>
2019-04-12 18:08:05 +1000

    added form and styling

--- client/src/components/video/VideoCreate.js
+++ client/src/components/video/VideoCreate.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class VideoCreate extends React.Component {
-  renderInput(formProps) {
-    console.log(formProps);
-    return <input type="text" value={formProps.value} onChange={formProps.input.onChange} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
       <div>
-        <form>
-          <Field name="title" component={this.renderInput} />
-          <Field name="description" component={this.renderInput} />
+        <form className="ui form">
+          <Field name="title" component={this.renderInput} label="Enter Title" />
+          <Field name="description" component={this.renderInput} label="Enter Description" />
         </form>
       </div>
     );
